88bd0fa3260ccd0e04440ce940b207c776f34214
locoda <locoda@foxmail.com>
2017-06-29 19:45:09 +0800

    add a sample read file

---
+++ sina_crawler/test_read.py
@@ -0,0 +1,13 @@
+import json
+import ast
+
+
+with open('2718604160_fans.txt', "r") as f:
+    fan_set = set([])
+    for line in f:
+        data = ast.literal_eval(line)
+        print data
+        fan_set = fan_set.union(data)
+
+    print fan_set
+    print len(fan_set)

b9d0b702d569d5f21b1dc124c55e2b77c500637d
He Huang <yimi@ip-172-31-19-252.ap-northeast-1.compute.internal>
2017-12-16 20:07:45 +0000

    update postprocess

--- twitter_crawler/postprocess.sh
+++ twitter_crawler/postprocess.sh
@@ -1,3 +1,9 @@
+cd ~/smalldata/twitter_crawler
+# export form mongo
 mongoexport --db kol --collection info --type=csv --fieldFile fields.txt --out data/kol.csv
+# change headline and add BOM
 sed -i "1 s/^.*$/昵称, 用户名, 简介, 粉丝, 关注, 推文数, 喜欢数/" data/kol.csv
 sed -i '1s/^\(\xef\xbb\xbf\)\?/\xef\xbb\xbf/' data/kol.csv
+# copy to onedrive and sync
+cp data/kol.csv ~/OneDrive
+onedrive -v
